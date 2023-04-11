adf9f4f49ead64ad378e52c4d705597e2820266d
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-11 12:37:34 -0300

    I think that I have finished the category routes. Moving on to the tags routes.

--- Develop/routes/api/category.js
+++ Develop/routes/api/category.js
@@ -51,34 +51,41 @@ router.post('/', async (req, res) => {
 
 // PUT /api/categories/:id to update category by its `id` value
 router.put('/:id', async (req, res) => {
+    const categoryId = req.params.id;
+    console.log(req.body);
     try {
-        Category.update({
-
-        },
-        {
-            where: {
-                id: req.params.id,
-            }
-        }).then((updatedID) => {
-            res.json(updatedID);
-        })
+       const updateCategory = await Category.update({ name: req.body.category_name},
+            {
+                where: {
+                    id: categoryId,
+                },
+            });
+            if (updateCategory[0] === 0){
+                res.status(404).json({ message: `No categories found matching id`});
+            } else {
+           res.status(200).json({ message: `successfully updated category` });
+            };
     } catch (error) {
-
+        res.status(500).json(error);
     }
 });
 
 // DELETE /api/categories/:id to delete a category by its `id` value
 router.delete('/:id', async (req, res) => {
+    const categoryId = req.params.id
     try {
-        Category.destroy({
+       const deleteCategory = await Category.destroy({
             where:{
-                id: req.params.id
+                id: categoryId
             },
-        }).then ((deletedCategory) => {
-            res.json(deletedCategory);
         })
+        if (deleteCategory0 === 0){
+            res.status(404).json({ message: `Category with ID ${categoryId} not found` });
+        } else {
+            res.status(200).json({ message: `Successfully deleted category with ${categoryID}`, deletedRecords: deleteCategory});
+        }
     } catch (error) {
-
+        res.status(500).json(error);
     }
 });
 
--- Develop/routes/api/product.js
+++ Develop/routes/api/product.js
@@ -114,7 +114,7 @@ router.delete('/:id', async (req, res) => {
         id: productID,
       },
     });
-  if (productToDestroy === 0) {
+  if (productToDestroy0 === 0) {
     res.status(404).json({ message: `Product with ID ${productID} not found` });
   } else {
     res.status(200).json({ message: `Successfully deleted product with ${productID}`});
